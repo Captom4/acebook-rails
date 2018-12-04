4c7e9ee749e0bd6e7cc1cdb819187bae0f48966b
Tom Hird <tombjh@hotmail.co.uk>
2018-12-04 21:37:53 +0000

    added post complex html and navigation layout

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,13 @@
 module PostsHelper
+  def post_date(post)
+    if post.updated_at.equal?(post.created_at)
+      return post.created_at
+    else
+      return post.updated_at
+    end
+  end
+
+  def created_by(post)
+    post.user.email
+  end
 end
